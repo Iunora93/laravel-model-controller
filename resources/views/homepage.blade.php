f6635b5239868f48b5131046f40458df03b75c44
Iunora93 <eleonoraruggieri22@gmail.com>
2023-01-16 16:48:26 +0100

    Passa database alla view, stampa a schermo

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel-Movies</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -22,7 +22,17 @@
     </head>
     <body>
         <main>
-            <h1>Home page</h1>
+            <h1>Movies</h1>
+            <ul>
+                @foreach ($movies as $movie)
+                  <li><h3>{{$movie->title}}</h3></li>
+                    <h5>{{$movie->original_title}}</h5>
+                    <small>{{$movie->nationality}}</small> <br>
+                    <small>{{$movie->date}}</small>
+                    <h3>Voto: {{$movie->vote}}</h3>
+
+                @endforeach
+            </ul>
         </main>
     </body>
 </html>
